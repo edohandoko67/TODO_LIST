684c2f5f1a33e0ac2af8ddd90322f4eeebda0f3a
EdoHand <edohandoko65@gmail.com>
2023-02-12 16:40:32 +0700

    Menambahkan add task ke dalam firestore

--- app/src/main/java/com/example/todolist/AddNewTask.java
+++ app/src/main/java/com/example/todolist/AddNewTask.java
@@ -1,22 +1,46 @@
 package com.example.todolist;
 
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddNewTask extends BottomSheetDialogFragment {
     static final String TAG = "AddNewTask";
     private TextView setDue;
     private EditText etTask;
     private Button btnSave;
+    private FirebaseFirestore firestore;
+    private Context context;
+    private String dueDate;
     public static AddNewTask newInstance(){
         return new AddNewTask();
     }
@@ -35,6 +59,97 @@ public class AddNewTask extends BottomSheetDialogFragment {
         etTask = view.findViewById(R.id.etTask);
         btnSave = view.findViewById(R.id.buttonSave);
 
+        firestore = FirebaseFirestore.getInstance();
+        etTask.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().equals("")){
+                    btnSave.setEnabled(false);
+                    btnSave.setBackgroundColor(Color.BLUE);
+                }else {
+                    btnSave.setEnabled(true);
+                    btnSave.setBackgroundColor(getResources().getColor(R.color.dark_green));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        setDue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int DAY = calendar.get(Calendar.DATE);
+                int MONTH = calendar.get(Calendar.MONTH);
+                int YEARS = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    month = month + 1;
+                    setDue.setText(dayOfMonth + "/" + month + "/" + year);
+                    dueDate = dayOfMonth + "/" + month + "/" + year;
+                    }
+                }, DAY, MONTH, YEARS);
+                datePickerDialog.show();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = etTask.getText().toString();
+
+                if (task.isEmpty()){
+                    Toast.makeText(context, "Harus Terisi!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Map<String, Object> taskMap = new HashMap<>();
+                    taskMap.put("task" ,task);
+                    taskMap.put("date" ,dueDate);
+                    taskMap.put("status" ,0);
+
+                    firestore.collection("task").add(taskMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(context, "Task Saved", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(context, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Activity activity = getActivity();
+        if (activity instanceof OnDialogCloseListener){
+            ((OnDialogCloseListener)activity).onDialog(dialog);
+        }
     }
 }
